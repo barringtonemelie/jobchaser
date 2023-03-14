7bb0cc1916c5c2b04a0961bc2775b97bd70d8d96
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-14 10:55:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jobchaser
+ 
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Jobchaser</h1>
+    </div>
+  )
+}
+
+export default App
