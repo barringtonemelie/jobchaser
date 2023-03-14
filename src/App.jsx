f60d69c0ff6cd2af75820f0674fb0e7a14df8e6c
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-14 13:33:59 +0100

    Started working on importing JSON data
    Co-authored-by: FernandoM87 <FernandoM87@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,75 @@ import './css/App.css'
 
 function App() {
 
+  const [jobData, setJobData] = React.useState("");
+
+  let job = {
+    fetchJSON: function () {
+      fetch("./src/data/data.json")
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+    },
+    handleJSON: function (data) {
+      console.log(data)
+    }
+  }
+
+  
+  job.fetchJSON();
+
+  setJobData((prevData) => {
+    return job.fetchJSON();
+  })
+
+
   return (
     <div className="App">
       <h1>Jobchaser</h1>
+      <JobList />
+      {
+        // async function getJobData () {
+        //   fetch("./src/data/data.json")
+        //   .then((response) => response.json())
+        //   .then((data) => {
+        //     <JobList data={data}/>
+        //   })
+        //   .catch((err) => console.error(err))
+        // }
+      }
     </div>
+  )
+}
+
+// Importing JSON
+
+// testing
+
+//
+
+
+
+//Complete list of jobs
+function JobList() {
+  //Test
+  
+
+  React.useEffect(() => {
+    
+  })
+
+  return(
+    <ul>
+      
+      <JobItem />
+    </ul>
+  )
+}
+
+
+//Separate job list items 
+function JobItem() {
+  return(
+    <li></li>
   )
 }
 
