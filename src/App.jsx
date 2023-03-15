b56d0c0f2f9b5056afc1770e794e519b278df94d
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-15 10:53:31 +0100

    Finished generating job data from JSON file
    Co-authored-by: FernandoM87 <FernandoM87@users.noreply.github.com>
    Co-authored-by: Abubakar Abdulkader <abubakarabdulkader21@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,75 +3,62 @@ import './css/App.css'
 
 function App() {
 
-  const [jobData, setJobData] = React.useState("");
-
-  let job = {
-    fetchJSON: function () {
-      fetch("./src/data/data.json")
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-    },
-    handleJSON: function (data) {
-      console.log(data)
-    }
-  }
-
   
-  job.fetchJSON();
-
-  setJobData((prevData) => {
-    return job.fetchJSON();
-  })
-
 
   return (
     <div className="App">
       <h1>Jobchaser</h1>
       <JobList />
-      {
-        // async function getJobData () {
-        //   fetch("./src/data/data.json")
-        //   .then((response) => response.json())
-        //   .then((data) => {
-        //     <JobList data={data}/>
-        //   })
-        //   .catch((err) => console.error(err))
-        // }
-      }
     </div>
   )
 }
 
-// Importing JSON
-
-// testing
-
-//
-
 
 
 //Complete list of jobs
-function JobList() {
-  //Test
-  
+function JobList() {  
+
+  const [jobData, setJobData] = React.useState([]);
 
   React.useEffect(() => {
-    
-  })
 
+      async function getData() {
+        const response = await fetch("./src/data/data.json")
+        .then((response) => {
+        return response.json()
+        // let data = response.json()
+        // console.log(data); 
+        // setJobData(data);
+      })
+      .catch((error) => {
+        console.error(error); 
+      })
+
+      setJobData(response); 
+    }
+
+    getData(); 
+    
+  }, [])
+
+  console.log("Job data: ", jobData); 
   return(
     <ul>
+      {jobData.map((data) => <JobItem data={data} /> )}
       
-      <JobItem />
     </ul>
   )
 }
 
 
 //Separate job list items 
-function JobItem() {
+function JobItem(props) {
   return(
-    <li></li>
+    <li>
+      <h2 className="" >{props.data.company}</h2>
+      <p>{props.data.description}</p>
+      <p>{props.data.tags}</p>
+    </li>
   )
 }
 
