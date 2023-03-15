04cb66fab4d1057cb2e7accba7fdafbe1bdf63fe
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-15 11:10:29 +0100

    Installed tailwind
    Co-authored-by: FernandoM87 <FernandoM87@users.noreply.github.com>
    Co-authored-by: Abubakar Abdulkader <abubakarabdulkader21@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Jobchaser</h1>
+      <h1 className="text-3xl font-bold underline">Jobchaser</h1>
       <JobList />
     </div>
   )
