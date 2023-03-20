58758bcad1917f8295219b151c90090e621dac97
abubakar <abubakarabdulkader21@gmail.com>
2023-03-20 14:58:41 +0100

    code cleaning in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import './css/App.css'
 import { JobItemm, SearchBar } from './components/index.jsx';
 
 function App() {
@@ -12,9 +11,6 @@ function App() {
         const response = await fetch("./src/data/data.json")
         .then((response) => {
         return response.json()
-        // let data = response.json()
-        // console.log(data); 
-        // setJobData(data);
       })
       .catch((error) => {
         console.error(error); 
@@ -27,21 +23,10 @@ function App() {
     
   }, [])
 
-  console.log("Job data: ", jobData); 
+  // console.log("Job data: ", jobData); 
   
-
   return (
-  //   <div className="App">
-  //     <h1 className="text-3xl font-bold underline">Jobchaser</h1>
-  //     { <JobList /> }
-      
-  //  </div>
-    
 
-    // <ul>
-    //   {jobData.map((data) => <JobItemm data={data} key={data.id}/>)}
-    // </ul>
-  
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ width: '65%'}}>     
         <SearchBar />
@@ -52,55 +37,5 @@ function App() {
     </div>
   )
 }
-
-
-
-//Complete list of jobs
-// function JobList() {  
-
-//   const [jobData, setJobData] = React.useState([]);
-
-//   React.useEffect(() => {
-
-//       async function getData() {
-//         const response = await fetch("./src/data/data.json")
-//         .then((response) => {
-//         return response.json()
-//         // let data = response.json()
-//         // console.log(data); 
-//         // setJobData(data);
-//       })
-//       .catch((error) => {
-//         console.error(error); 
-//       })
-
-//       setJobData(response); 
-//     }
-
-//     getData(); 
-    
-//   }, [])
-
-//   console.log("Job data: ", jobData); 
-//   return(
-//     <ul>
-//       {jobData.map((data) => <JobItemm data={data} />)}
-      
-//     </ul>
-//   )
-// }
-
-
-//Separate job list items 
-// function JobItem(props) {
-//   return(
-//     <li>
-//       {console.log(props.data)}
-//       <h2 className="" >{props.data.company}</h2>
-//       <p>{props.data.description}</p>
-//       <p>{props.data.tags}</p>
-//     </li>
-//   )
-// }
 
 export default App
