7a150754ef548e42b90f52b94d613cea21fffffc
Emelie Barrington <emelie.barrington@chasacademy.se>
2023-03-23 13:57:21 +0100

    Added search functionality
    Co-authored-by: 02kgsja@gmail.com <02kgsja@gmail.com>
    Co-authored-by: Abubakar Abdulkader <abubakarabdulkader21@gmail.com>
    Co-authored-by: FernandoM87 <FernandoM87@users.noreply.github.com>

--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -2,49 +2,47 @@ import { BsSearch, BsFilter } from 'react-icons/bs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { useDispatch, useSelector } from "react-redux"
+import { setSearchInput, filterSearch } from ".././store/slices/dataSlice"
 import { JobItem } from './index';
 
 function SearchBar() {
   const jobs = useSelector((state) => state.data.value); 
   const [search, setSearch] = useState();
-  const keys = ["description", "company", "tags"]
+  const dispatch = useDispatch()
+  // const keys = ["description", "company", "tags"]
 
-  const searchFiltering = (jobs) => {
-    return jobs.filter(
-      (item) => {
-        console.log(keys.some(key => item[key].includes(search)))
-        if (keys.some(key => item[key].includes(search))) {
-          console.log(item);
-          return <JobItem data={item} key={item.id} /> // det här borde inte funka men retunera item istället o ta bort kommentar i rad 32 o kommentera rad 31
-        }
+  // const searchFiltering = (jobs) => {
+  //   return jobs.filter(
+  //     (item) => {
+  //       console.log(keys.some(key => item[key].includes(search)))
+  //       if (keys.some(key => item[key].includes(search))) {
+  //         console.log(item);
+  //         return <JobItem data={item} key={item.id}/>;
+  //       }
       
-      }
-        )
-  }
+  //     }
+  //       )
+  // }
   // console.log(jobs);
+
+  function handleChange(value) {
+    setSearch(value);
+  }
+
+
   return (
 
     <div className="input-group mb-5 mt-5">
           <div className="input-group-prepend">
             <button onClick={() => {
               console.log("Searched " + search);
-              // console.log(<JobItem data={search(jobs) } key={search(jobs).id} />);
-              <JobItem data={searchFiltering(jobs) } key={search.id} />
-
-            //     <JobItem data={search(jobs) } key={index.id} />
-
-            //   {jobs.filter((theJob) => {
-            //     return search.toLowerCase() === '' ? theJob : theJob.company.toLowerCase().includes(theJob)
-            //   }).map((theJob) => (
-            
-            //   ))
-            // }
-              // <JobItem data={search(jobs)} key={item.id} />
+              dispatch(setSearchInput(search));
+              dispatch(filterSearch()); 
             }} className="btn btn-primary h-100" type="button" style={{backgroundColor: '#0d6efd'}} >
               <BsSearch />
             </button>
           </div>
-          <input type="text" className="form-control rounded-3 border-success" placeholder="Search" aria-label="Search" style={{ marginRight: '10px', marginLeft: '10px' }} onChange={(e) => setSearch(e.target.value)} />
+          <input type="text" className="form-control rounded-3 border-success" placeholder="Search" aria-label="Search" style={{ marginRight: '10px', marginLeft: '10px' }} onChange={(e) => handleChange(e.target.value)} />
           <div className="input-group-append">
             <button className="btn btn-outline-secondary h-100" type="button">
               <BsFilter />
--- src/store/slices/dataSlice.js
+++ src/store/slices/dataSlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit"
 
+const keys = ["description", "company", "tags"]
 
 const initialState = {
-    value: []
+    value: [],
+    searchedJobs: [],
+    currentSearch: ""
 }
 
 const jobSlice = createSlice({
@@ -11,11 +14,30 @@ const jobSlice = createSlice({
     reducers: {
         setJobs: (state, action) => {
             state.value = action.payload;
+        },
+        setSearchInput: (state, action) => {
+            state.currentSearch = action.payload;
+            console.log(state.currentSearch); 
+        },
+        filterSearch: (state, action) => {
+            
+            state.searchedJobs = state.value.filter(
+                (job) => {
+                  console.log(keys.some(key => job[key].includes(state.currentSearch)))
+                  if (keys.some(key => job[key].includes(state.currentSearch))) {
+                    console.log(job);
+                    return job; 
+                  }
+                }
+                
+            )
+
+            console.log("State", state.value); 
         }
-        //filterJobs: (state, action) => {
-        // }
     }
 })
 
 export const { setJobs } = jobSlice.actions
+export const { setSearchInput } = jobSlice.actions
+export const { filterSearch } = jobSlice.actions
 export default jobSlice.reducer
